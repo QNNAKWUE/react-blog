276a2daaddfe8a64e67d9b23bb8b80e9c7dd45ef
Queen Nnakwue <qnnakwue@gmail.com>
2021-09-22 22:27:09 +0100

    updating changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+
 import Home from './Components/Home';
 
 
 function App() {
   return (
+    
       <div className="App">
             <Home />
       </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,32 +3,29 @@ import "../styles/Home.css"
 
 function Home() {
     return (
+
         <div className='Home'>
-            <div className='home__body'>
-
-            <div className='home__header'>
                 <h2>Nicole's Blog.</h2>
-
-                <div className='home__headerList'>
-                    <ul>
-                            <li>
-                            Register
-                            </li>
-
-                            <li>
-                                Login
-                            </li>
-                            
-                            <li>
-                                Write a Story
-                            </li>
-                        </ul>
-                </div>
-                   
-            </div>
             
-               
+                <div className='navbar__list'>
+                    <ul>
+                        <li>
+                            Register
+                        </li>
 
+                        <li>
+                            Login
+                        </li>
+                        
+                        <li>
+                           Write a Story
+                        </li>
+                    </ul>
+                </div>
+            
+
+        
+            <div className='home__body'>
                     <img className="home__image"
                     src='https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
                     alt='splash'
